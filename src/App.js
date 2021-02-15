6686ed1353826b87298cb6114f4127f5f69ea4bc
Randy Roberson <Randyhr318@gmail.com>
2021-02-14 19:27:48 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, {useState} from "react"; 
 import Tweet from './Tweet';
+
 function App(){
-
- const [isRed, setRed] = useState(false);
- const [count, setCount] = useState(0);
-//Line 12 is rendering component
-
-const increment = () => {
-  setCount(count + 1);
-  setRed(!isRed);
-};
+const [users, setUsers] = useState([
+  {name:"Ed", message: "Hello there"},
+  {name: "John", message: "I am John snow"},
+  {name: "traversy", message: "I am awesome"}
+]);
+ 
+  
   return( //javascript(JSX) compiled to html. curly braces allow javascript
     <div className="app">
-      <h1 className={isRed ? 'red' : ''}>Change my color!</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1> 
+       {users.map(user => (
+         <Tweet name={user.name} message ={user.message} />
+       ))}
     </div>
   );
 }
